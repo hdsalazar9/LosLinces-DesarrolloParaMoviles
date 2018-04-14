f7eb3583df51bb96df0600377d8d596aca8ded3d
Jesus Alatorre <chuyalatorre@gmail.com>
2018-04-13 23:08:38 -0500

    MainMenu switching

--- app/src/main/java/itesm/mx/saludintegral/MenuItem.java
+++ app/src/main/java/itesm/mx/saludintegral/MenuItem.java
@@ -1,8 +1,29 @@
 package itesm.mx.saludintegral;
 
 /**
- * Created by Jesus on 4/13/2018.
+ * Clase MenuItem:
+ * Contiene la clase de los items de menu que se utilizarán
+ * en las distintas pantallas de selección/menu de la aplicación.
+ *
+ * Contiene un título (nombre de la selección),
+ * y un int referente al id de Drawable de la imagen
+ * a desplegar en el boton.
  */
 
 public class MenuItem {
+    private String sTitle;
+    private int iImage;
+
+    MenuItem(String sTitle, int iImage){
+        this.sTitle=sTitle;
+        this.iImage=iImage;
+    }
+
+    String getTitle(){
+        return this.sTitle;
+    }
+
+    int getImage(){
+        return this.iImage;
+    }
 }
