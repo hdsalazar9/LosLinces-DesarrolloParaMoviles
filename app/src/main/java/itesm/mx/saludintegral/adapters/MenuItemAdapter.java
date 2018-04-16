93203ae521f7726254aefa80e216cbcc2c3e36df
HectorSalazar <h.d.salazar@hotmail.com>
2018-04-16 18:44:47 -0500

    Add color background to MainMenu

--- app/src/main/java/itesm/mx/saludintegral/adapters/MenuItemAdapter.java
+++ app/src/main/java/itesm/mx/saludintegral/adapters/MenuItemAdapter.java
@@ -1,11 +1,13 @@
 package itesm.mx.saludintegral.adapters;
 
 import android.content.Context;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -36,9 +38,29 @@ public class MenuItemAdapter extends ArrayAdapter<MenuItem> {
 
         TextView tvItemTitle = convertView.findViewById(R.id.tvItemTitle);
         ImageView ivMenuImage = convertView.findViewById(R.id.ivItemImage);
+        LinearLayout linearLayout = convertView.findViewById(R.id.ll_menu_row);
 
         tvItemTitle.setText(mItem.getTitle());
         ivMenuImage.setImageResource(mItem.getImage());
+
+        switch (mItem.getTitle()){
+            case "Salud":
+                linearLayout.setBackgroundColor(ResourcesCompat.getColor(
+                        getContext().getResources(),R.color.colorSalud, null));
+                break;
+            case "Social":
+                linearLayout.setBackgroundColor(ResourcesCompat.getColor(
+                        getContext().getResources(),R.color.colorSocial, null));
+                break;
+            case "Ejercicio":
+                linearLayout.setBackgroundColor(ResourcesCompat.getColor(
+                        getContext().getResources(),R.color.colorEjercicio, null));
+                break;
+            case "Espiritual":
+                linearLayout.setBackgroundColor(ResourcesCompat.getColor(
+                        getContext().getResources(),R.color.colorEspiritual, null));
+                break;
+        }
 
         return convertView;
     }
