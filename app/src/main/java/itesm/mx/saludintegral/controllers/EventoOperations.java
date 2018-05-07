f2f679af96c008cf7926d81f57dd7684597534b4
Fernando David Romero Nava <ferdav19@gmail.com>
2018-05-07 17:15:39 -0500

    Commit forzado para poder bajar cambios

--- app/src/main/java/itesm/mx/saludintegral/controllers/EventoOperations.java
+++ app/src/main/java/itesm/mx/saludintegral/controllers/EventoOperations.java
@@ -143,7 +143,6 @@ public class EventoOperations {
     }
 
 
-
     public boolean deleteEvento(String nombreDeEventoABorrar, Date fechaEvento){
         boolean result = false;
         String query="SELECT * FROM "+DataBaseSchema.EventosTable.TABLE_NAME+" WHERE "+
