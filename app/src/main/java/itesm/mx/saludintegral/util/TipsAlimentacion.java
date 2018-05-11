4c0bac7e600c041d134c5f9e781917e00da697b9
Jose_Cruz <josecruz.9807@gmail.com>
2018-05-11 01:04:34 -0500

    Arreglo a layout de alimentacion

--- app/src/main/java/itesm/mx/saludintegral/util/TipsAlimentacion.java
+++ app/src/main/java/itesm/mx/saludintegral/util/TipsAlimentacion.java
@@ -1,29 +1,52 @@
 package itesm.mx.saludintegral.util;
 
+import itesm.mx.saludintegral.R;
+
 /**
  * Created by josec on 07/05/2018.
  */
 
 public class TipsAlimentacion {
     public static String sTips[]={
-            "De queso, yogur, natillas, etc. se recomienda consumir tres raciones al día. La leche y sus derivados son fuente de calcio, vitaminas A, D, B2, proteínas de alto valor biológico y grasas.",
-            "Los aceites vegetales contienen vitamina E y las mantequillas, vitaminas A y D. En crudo pueden utilizarse todos los aceites, pero para cocinar se aconseja el aceite de oliva o el de girasol (contienen ácido oleico en elevada proporción) porque resisten elevadas temperaturas.",
-            "Se recomiendan dos raciones de carnes, pescado, huevos o algún derivado al día, intentando alternar los alimentos. Este grupo es fuente de proteínas, grasas, hierro, vitaminas B1, B2, niacina y ácido fólico.",
-            "Se recomienda consumir 4 o 5 raciones de cereales (y sus derivados: pan, pastas, galletas), legumbres y tubérculos (sobre todo, patata) al día. Las legumbres pueden consumirse dos veces a la semana.",
+            "Se recomienda beber agua en los intervalos de las comidas, de 2 a 3 litros diarios.",
+            "Se recomiendan comer tres raciones al día de: \n+Queso\n+Yogur\n+Natillas\n\nLa leche y sus derivados son fuente de calcio, vitaminas A, D, B2, proteínas de alto valor biológico y grasas.",
+            "+Los aceites vegetales contienen vitamina E\n+Las mantequillas, vitaminas A y D. \n\nEn crudo pueden utilizarse todos los aceites, pero para cocinar se aconseja el aceite de oliva o el de girasol (contienen ácido oleico en elevada proporción) porque resisten elevadas temperaturas.",
+            "Se recomiendan dos raciones al día de: \n+Carnes \n+Pescado \n+Huevos \n+Algún otro derivado \n\nintentando alternar los alimentos. Este grupo es fuente de proteínas, grasas, hierro, vitaminas B1, B2, niacina y ácido fólico.",
+            "Se recomienda consumir 4 o 5 raciones diarias de:\n+Cereales (y sus derivados: pan, pastas, galletas) \n+Legumbres\n+Tubérculos (sobre todo, patata) \n\nLas legumbres pueden consumirse dos veces a la semana.",
             "Se recomienda ingerir diariamente dos raciones de verduras u hortalizas (preferiblemente una ración cocida y otra cruda, por ejemplo en forma de ensalada) y dos piezas de fruta (una de ellas rica en vitamina C: cítricos, fresas y melón).",
             "Las verduras, frutas y hortalizas están compuestas en un 80% por agua y el resto por vitaminas, minerales y fibra vegetal.",
             "Se recomienda ingerir 30-50 gramos de alimentos grasos por día, preferiblemente derivados de aceites vegetales.",
             "Conviene no abusar de grasas de origen animal como la mantequilla, la manteca de cerdo o la nata, porque son ricas en ácidos grasos saturados y colesterol. Los frutos secos (nueces, almendras, piñones, etc.) se diferencian de las frutas frescas por su contenido en grasa.",
             "Se toleran uno o dos vasos pequeños de vino al día. Otras bebidas de alta graduación están contraindicadas, ya que en las personas mayores disminuye la actividad de los sistemas enzimáticos encargados de la degradación del alcohol.",
             "Se recomienda disminuir el consumo de té y café porque alteran el sueño y son diuréticos, es decir contribuyen a la deshidratación, en especial cuando se toma poco líquido.",
-            "Se recomienda beber agua en los intervalos de las comidas.",
             "La leche o el yogur son necesarios en todas las etapas de la vida, porque contienen proteínas para reparar y renovar tejidos y calcio, esencial para mantener los huesos sanos y prevenir la osteoporosis.",
-            "Se recomienda disminuir el consumo de alimentos preparados con mucha sal, como los alimentos enlatados, las galletas saladas y las comidas preparadas.",
+            "Se recomienda disminuir el consumo de alimentos preparados con mucha sal, como:\n+Alimentos enlatados\n+Galletas saladas\n+Comidas preparadas.",
             "Se recomienda ingerir alimentos ricos en fibra, para mejorar la función intestinal.",
-            "Se puede incluir, con la asesoría de un experto, suplementos vitamínicos como calcio, vitamina D, B, ácido fólico, magnesio, hierro, etc.",
+            "Se puede incluir, con la asesoría de un experto, suplementos vitamínicos como:\n+Calcio\n+Vitamina D\n+Vitamina B\n+Ácido fólico\nMagnesio\nHierro",
             "Se recomienda reduicr la ingesta de carbohidratos simples, ya que pueden desarrollar aumento de la azúcar en la sangre.",
             "Cocinar no sólo tomando en cuenta los requerimientos nutricionales, sino también los gustos y deseos.",
             "Información obtenida de: Sanz, S. S. (2001). Orientaciones dietéticas en geriatría .Nutrición específica. El Sevier - Farmacia Profesional, 15, 4th ser., 68-73. Recuperado de http://www.elsevier.es/es-revista-farmacia-profesional-3-articulo-orientaciones-dieteticas-geriatria-nutricion-especifica-12003985",
             "Y de: E. (2016, March 22). Consejos para una buena nutrición en el adulto mayor. Recuperado en mayo 07, 2018, from http://www.socgeriatria.cl/site/?p=367"
+    };
+    public static int iResources[]={
+            R.drawable.agua,
+            R.drawable.lacteos,
+            R.drawable.aceite,
+            R.drawable.carnes,
+            R.drawable.cereales,
+            R.drawable.frutas,
+            R.drawable.frutas,
+            R.drawable.grasas,
+            R.drawable.carnes,
+            R.drawable.vino,
+            R.drawable.cafe,
+            R.drawable.lacteos,
+            R.drawable.salados,
+            R.drawable.cereales,
+            R.drawable.suplementos,
+            R.drawable.salados,
+            R.drawable.chef,
+            R.drawable.libro,
+            R.drawable.libro
     };
 }
